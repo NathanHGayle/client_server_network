132e597220968b748313037c89b0d9d35b5d0094
NateBI <nathanhg5@gmail.com>
2024-03-05 10:45:42 +0000

    Response from Server complete

--- client.py
+++ client.py
@@ -5,10 +5,11 @@
 # from task_functions import create_text_file
 # from task_functions import send_file
 from task_functions import find_from_config
+import socket
 from server_functions import initialise_connection
-
 # client sever constants
-CLIENT_SEVER = find_from_config('client_details', 'CLIENT_SEVER')
+# CLIENT_SEVER = find_from_config('client_details', 'CLIENT_SEVER')
+CLIENT_SEVER = socket.gethostbyname(socket.gethostname())
 FORMAT = find_from_config('client_details', 'FORMAT')
 HEADER = find_from_config('client_details', 'HEADER')
 DISCONNECT_MESSAGE = find_from_config('server_details', 'DISCONNECT_MESSAGE')
@@ -21,6 +22,7 @@ def client_send(msg,connection):
     send_length += b' ' * (HEADER - len(send_length))
     connection.send(send_length)
     connection.send(message)
+    print(connection.recv(2048).decode(FORMAT))
 
 def main():
     client_activate = initialise_connection()
